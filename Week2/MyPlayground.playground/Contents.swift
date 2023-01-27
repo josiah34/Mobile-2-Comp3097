c7fe1fabae14452698402b5f54c9e4fa1018ead2
mac <mac@macs-iMac-Pro.local>
2023-01-27 18:37:51 -0500

    Nearly Finished Lab 2

--- Week2/MyPlayground.playground/Contents.swift
+++ Week2/MyPlayground.playground/Contents.swift
@@ -24,3 +24,62 @@ func area2(_ w:Int, _ h:Int)->Int{
 }
 
 area2(1, 3)
+
+func multiMany(first:Int = 10,numbers:Int...)->Int{
+    var res = first
+    
+    for n in numbers{
+        res *= n
+    }
+    
+    
+    return res
+    
+}
+
+print(multiMany(numbers: 2,3,5,6,7))
+
+
+func foo(arr:[Int])->(min:Int, max:Int)?{
+    if arr.isEmpty {
+        return nil
+    }
+    
+    var curMin = arr[0] , curMax = arr[0]
+    
+    for v in arr[1..<arr.count]{
+        if v<curMin {
+            curMin = v
+        }
+        else if v>curMax{
+            curMax = v
+        }
+    }
+    return(curMin,curMax)
+}
+
+foo(arr: [1,6,71,8,45,55])
+foo(arr:    [])
+
+// function without return statement
+func boo()->String{
+    "test"
+}
+
+boo()
+
+
+func swap(n1:inout Int, n2:inout Int){
+    (n1, n2) = (n2,n1)
+}
+var x = 1, y = 4
+
+swap(n1: &x, n2: &y)
+x
+y
+
+
+func calculator(a:Int, b:Int, op:String)->Int{
+    
+}
+}
