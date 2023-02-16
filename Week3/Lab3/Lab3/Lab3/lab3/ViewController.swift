ff4039e6a85a25f399e82e4cba9f1f8194961504
mac <mac@macs-iMac-Pro.local>
2023-02-15 20:46:21 -0500

    Finished Lab Week 3

--- Week3/Lab3/Lab3/Lab3/lab3/ViewController.swift
+++ Week3/Lab3/Lab3/Lab3/lab3/ViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    var counter = 0
+    var step = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        label.text = "\(counter)"
+        
     }
 
 
+    @IBAction func addOne(_ sender: UIButton) {
+        counter+=step
+        label.text = "\(counter)"
+    }
+    
+    
+
+    @IBAction func minusOne(_ sender: UIButton) {
+        counter-=step
+        label.text = "\(counter)"
+        
+    }
+    
+    
+    @IBAction func clear(_ sender: UIButton) {
+        counter=0
+        label.text = "\(counter)"
+        
+    }
+    
+    @IBAction func stepUp(_ sender: UIButton) {
+        step+=1
+        sender.setTitle("Increase Step (\(step))", for: .normal)
+    }
+    
 }
 
