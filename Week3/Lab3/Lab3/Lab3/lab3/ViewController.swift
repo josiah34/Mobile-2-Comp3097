3dd5953c02ec77c4606463ec18b366495fd7adfd
mac <mac@macs-iMac-Pro.local>
2023-02-15 19:14:21 -0500

    Started Lab Week 3

---
+++ Week3/Lab3/Lab3/Lab3/lab3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab3
+//
+//  Created by mac on 2023-02-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
